23d50ac425345a269777b9c6157f2a08ecd3a15f
Jherome224 <otech2242003@gmail.com>
2026-04-26 11:47:35 +0800

    added components for better information graphing, mermaid, tabs, accordian

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -1,9 +1,10 @@
-// components/mdx.tsx
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 import { OfficerCarousel } from "@/components/officer-carousel";
+import { Mermaid } from '@/components/mdx/mermaid';
+import { Accordion, Accordions } from 'fumadocs-ui/components/accordion';
+import { Tab, Tabs } from 'fumadocs-ui/components/tabs';
 
-// 1. Remove 'async'
 // 2. Change 'Promise<MDXComponents>' to just 'MDXComponents'
 export function getMDXComponents(
   base: MDXComponents,
@@ -11,6 +12,11 @@ export function getMDXComponents(
 ): MDXComponents {
   return {
     ...defaultMdxComponents,
+    Mermaid,
+    Accordion,
+    Accordions,
+    Tab,
+    Tabs,
     ...base,
     ...(orgId
       ? {
